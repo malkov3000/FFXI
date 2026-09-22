e4aebd56518f1647d6ce6ddcfbc3ed172c15c5fd
malkov3000 <henry.maliszewski@gmail.com>
2026-09-22 11:07:17 -0500

    Update Themyscira_BLU.lua

--- Themyscira_BLU.lua
+++ Themyscira_BLU.lua
@@ -5,11 +5,10 @@
 ------------------------------------------------------------------
 
 
+
 ----------------
 ---- Load ------
 ----------------
-
-include('organizer-lib')
 
 ------------------------------------LOCKSTYLE------------------------------------------
 local lockstyle = 1 -- Uses the in-game gearsets! Set # to desired lockstyle look!
@@ -23,8 +22,73 @@ end
 send_command('input /macro book 1') -- Update # to desired starting macro book!
 send_command('wait 4; input /macro set 1') -- Update # to desired starting macro set!
 
---no help on!
-send_command('input /blockhelp on')
+-------------------------------------NO HELP ON----------------------------------------
+send_command('input /blockhelp on') -- Prevents calling for help!
+
+
+
+-------------------
+--   AREA List   --
+-------------------
+
+areas = {}
+
+-- City areas for town gear
+areas.towns = S{
+    "Ru'Lude Gardens",
+    "Upper Jeuno",
+    "Lower Jeuno",
+    "Port Jeuno",
+    "Port Windurst",
+    "Windurst Waters",
+    "Windurst Woods",
+    "Windurst Walls",
+    "Heavens Tower",
+    "Port San d'Oria",
+    "Northern San d'Oria",
+    "Southern San d'Oria",
+    "Port Bastok",
+    "Bastok Markets",
+    "Bastok Mines",
+    "Metalworks",
+    "Aht Urhgan Whitegate",
+    "Tavnazian Safehold",
+    "Nashmau",
+    "Selbina",
+    "Mhaura",
+    "Norg",
+    "Eastern Adoulin",
+    "Western Adoulin",
+    "Kazham",
+    "Rabao",
+    "Chocobo Circuit",
+}
+
+-------------------------
+--   BLU Spells List   --
+-------------------------
+
+blu_cure = S {'Healing Breeze', 'Magic Fruit', 'Plenilune Embrace', 'Pollen', 'Restoral', 'Wild Carrot'} 
+
+blu_phy = S {'Asuran Claws', 'Bilgestorm', 'Bludgeon', 'Body Slam', 'Feather Storm', 'Mandibular Bite', 'Queasyshroom', 'Power Attack', 'Ram Charge', 'Saurian Slide', 'Screwdriver', 'Sickle Slash', 
+'Smite of Rage', 'Spinal Cleave', 'Spiral Spin', 'Sweeping Gouge', 'Terror Touch', 'Battle Dance', 'Bloodrake', 'Death Scissors', 'Dimensional Death', 'Empty Thrash', 'Quadrastrike', 'Uppercut', 
+'Tourbillion', 'Thrashing Assault', 'Vertical Cleave', 'Whirl of Rage', 'Amorphic Spikes', 'Barbed Crescent', 'Claw Cyclone', 'Disseverment', 'Foot Kick', 'Frenetic Rip', 'Goblin Rush', 
+'Hysteric Barrage', 'Paralyzing Triad', 'Seedspray', 'Sinker Drill', 'Vanity Dive', 'Cannonball', 'Delta Thrust', 'Glutinous Dart', 'Grand Slam', 'Quad. Continuum', 'Sprout Smack', 
+'Benthic Typhoon', 'Helldive', 'Hydro Shot', 'Jet Stream', 'Pinecone Bomb', 'Wild Oats', 'Sweeping Gouge', 'Sudden Lunge'}
+
+blu_mab = S {'Acrid Stream', 'Anvil Lightning', 'Crashing Thunder', 'Charged Whisker', 'Droning Whirlwind', 'Firespit', 'Foul Waters', 'Gates of Hades', 'Leafstorm', 'Molting Plumage', 
+'Nectarous Deluge', 'Polar Roar', 'Regurgitation', 'Rending Deluge', 'Scouring Spate', 'Searing Tempest', 'Silent Storm', 'Spectral Floe', 'Subduction', 'Tem. Upheaval', 'Thermal Pulse', 
+'Thunderbolt', 'Uproot', 'Water Bomb', 'Atra. Libations', 'Blood Saber', 'Dark Orb', 'Death Ray', 'Eyes On Me', 'Blazing Bound', 'Evryone. Grudge', 'Palling Salvo', 'Tenebral Crush', 
+'Blinding Fulgor', 'Diffusion Ray', 'Ice Break', 'Magic Hammer', 'Rail Cannon', 'Retinal Glare', 'Sandspin', 'Vapor Spray'}
+				   
+blu_macc = S {'Dream Flower', 'Sheep Song','Feather Tickle','Light of Penance', 'MP Drainkiss', 'Entomb'}
+
+blu_skill = S {'Diamondhide', 'Metallic Body', 'Magic Barrier', 'Occultation', 'Atra. Libations', 'Digest', 'Blood Saber', 'Osmosis', 'Retinal Glare'}
+
+blu_rupt = S {'Barrier Tusk', 'Cocoon', 'Erratic Flutter', 'Harden Shell', 'Orcish Counterstance', 'Plasma Charge', 'Pyric Bulwark', 'Memento Mori', 'Nat. Meditation', 'Reactor Cool', 'Saline Coat', 
+'Feather Barrier', 'Refueling', 'Warm-Up', 'Zephyr Mantle', 'Reactor Cool', 'Plasma Charge', 'Amplification', 'Mighty Guard', 'Carcharian Verve', 'Magic Barrier'}
+					 
+blu_bre = S {'Bad Breath', 'Poison Breath', 'Radiant Breath', 'Frost Breath', 'Heat Breath', 'Thunder Breath', 'Wind Breath'}
 
 
 
@@ -37,7 +101,6 @@ function get_sets()
 -- Use "//gs export" in-game to export a file with your currently equiped gear!
 -- Find the file in Windower>addons>GearSwap>data>export folder!
 -- Copy the gear inside the {} and paste it into the correct set below!
-
 
 ----------------------- IDLE SETS ----------------------
 
@@ -125,7 +188,8 @@ function get_sets()
     neck={ name="Mirage Stole +2", augments={'Path: A',}},
     waist="Kentarch Belt +1",
     left_ear="Crep. Earring",
-    right_ear="Dedition Earring",
+    --right_ear="Dedition Earring",
+    right_ear="Cessance earring",
     left_ring="Murky Ring",
     right_ring="Epona's ring",
     back={ name="Rosmerta's Cape", augments={'DEX+20','Accuracy+20 Attack+20','DEX+10','"Dual Wield"+10','Phys. dmg. taken-10%',}},
@@ -141,7 +205,7 @@ function get_sets()
     neck={ name="Mirage Stole +2", augments={'Path: A',}},
     waist="Kentarch Belt +1",
     left_ear="Crep. Earring",
-    right_ear="Dedition Earring",
+    right_ear="Cessance earring",
     left_ring="Murky Ring",
     right_ring="Ilabrat Ring",
     back="Null shawl",
@@ -192,8 +256,8 @@ function get_sets()
     ammo="Ghastly Tathlum +1",
     head="Hashishin Kavuk +3",
     body="Assim. Jubbah +4",
-    hands="Jhakri Cuffs +2",
-    legs="Luh. Shalwar +4",
+    hands="Nyame Gauntlets",
+    legs="Nyame flanchard",
     feet="Nyame sollerets",
     neck="Sibyl Scarf",
     waist="Eschan Stone",
@@ -376,7 +440,7 @@ function get_sets()
     back={ name="Rosmerta's Cape", augments={'STR+20','Accuracy+20 Attack+20','STR+10','Weapon skill damage +10%','Phys. dmg. taken-10%',}},
 }
 	
-	--Magic Attack Bonus / Magic Accuracy
+	--Magic Attack Bonus / Magic Accuracy / INT
 	sets.blu.mab = {
     ammo="Ghastly Tathlum +1",
     head="Hashishin Kavuk +3",
@@ -446,24 +510,23 @@ function get_sets()
 
 	--brrrrreath
 	sets.blu.bre = {
-    ammo="Mavi tathlum",
+    ammo="Mavi Tathlum",
     head="Luhlaza Keffiyeh +4",
     body="Hashishin Mintan +3",
     hands="Hashi. Bazu. +3",
-    legs="Luh. Shalwar +4",
+    legs="Hashishin Tayt +3",
     feet="Hashi. Basmak +3",
-    neck="Sibyl Scarf",
-    waist="Eschan Stone",
-    left_ear="Hecate's Earring",
-    right_ear="Friomisi Earring",
-    left_ring="Mephitas's Ring +1",
-    right_ring="Metamor. Ring +1",
+    neck="Unmoving Collar +1",
+    waist="Plat. Mog. Belt",
+    left_ear="Alabaster Earring",
+    right_ear="Etiolation Earring",
+    left_ring="Murky Ring",
+    right_ring="Gelatinous Ring +1",
     back={ name="Rosmerta's Cape", augments={'INT+20','Mag. Acc+20 /Mag. Dmg.+20','INT+10','"Mag.Atk.Bns."+10','Spell interruption rate down-10%',}},
 }
 
 
 
-	
 end
 
 
@@ -479,10 +542,6 @@ end
 
 function precast(spell)
 
--- Cancel
-	if canceled then 
-		return 
-	end
 	
 -- Magic
 	if spell.action_type == 'Magic' then 
@@ -520,12 +579,6 @@ end
 ---
 
 function midcast(spell)
-
-
--- Cancel
-	if canceled then 
-		return 
-	end
 	
 -- Magic
 	if sets.ma[spell.skill] then 
@@ -580,11 +633,6 @@ end
 
 function aftercast(spell)
 
--- Cancel
-	if canceled then 
-		return 
-	end
-
 	if player.status == 'Engaged' then 
         equip(sets.tp[TP_Set_Names[TP_Index]]) 
 	elseif areas.towns:contains(world.area) then 
@@ -593,7 +641,9 @@ function aftercast(spell)
 		equip(sets.idle.dt) 
 	end
 	
-	equip(sets.wep[WEP_Set_Names[WEP_Index]]) 
+    if sets.wep and sets.wep[WEP_Set_Names[WEP_Index]] then
+        equip(sets.wep[WEP_Set_Names[WEP_Index]])
+	end
 
 end
 
@@ -612,18 +662,12 @@ function status_change(new, old)
 	else
 		equip(sets.idle.dt) 
 	end
-end
-
--- Zone changes
-windower.register_event('zone change', function(new_zone_id, old_zone_id)
-	if player.status == 'Engaged' then 
-        equip(sets.tp[TP_Set_Names[TP_Index]]) 
-	elseif areas.towns:contains(world.area) then 
-		equip(sets.idle.town)
-	else
-		equip(sets.idle.dt) 
+	
+    if sets.wep and sets.wep[WEP_Set_Names[WEP_Index]] then
+        equip(sets.wep[WEP_Set_Names[WEP_Index]])
 	end
-end)
+	
+end
 
 
 ---
@@ -637,6 +681,7 @@ function self_command(command)
         send_command('@input /echo ----- Engaged Set changed to -----> '..TP_Set_Names[TP_Index])
         equip(sets.tp[TP_Set_Names[TP_Index]]) 
     elseif command == 'togglewep' then 
+		equip({sub = "empty"}) 
         WEP_Index = WEP_Index +1 
         if WEP_Index > #WEP_Set_Names then WEP_Index = 1 end 
         send_command('@input /echo ----- Weapon Set changed to -----> '..WEP_Set_Names[WEP_Index])
@@ -654,67 +699,40 @@ end
 
 
 
--------------------
---   AREA List   --
--------------------
 
-areas = {}
+------------------------------------------------------------------
+-- TOP-LEVEL ENGINE REGISTER (Must stay at the absolute bottom) --
+------------------------------------------------------------------
 
--- City areas for town gear
-areas.towns = S{
-    "Ru'Lude Gardens",
-    "Upper Jeuno",
-    "Lower Jeuno",
-    "Port Jeuno",
-    "Port Windurst",
-    "Windurst Waters",
-    "Windurst Woods",
-    "Windurst Walls",
-    "Heavens Tower",
-    "Port San d'Oria",
-    "Northern San d'Oria",
-    "Southern San d'Oria",
-    "Port Bastok",
-    "Bastok Markets",
-    "Bastok Mines",
-    "Metalworks",
-    "Aht Urhgan Whitegate",
-    "Tavnazian Safehold",
-    "Nashmau",
-    "Selbina",
-    "Mhaura",
-    "Norg",
-    "Eastern Adoulin",
-    "Western Adoulin",
-    "Kazham",
-    "Rabao",
-    "Chocobo Circuit",
-}
+-- Isolated Event Handler
+-- This handles the zone change safely without nesting bugs or scope crashes
+function handle_zone_gear_swap(new_zone_id, old_zone_id)
+    -- Safety validation gate: prevents indexing nil 'areas' values
+    if areas and areas.towns and player then
+        if player.status == 'Engaged' then 
+            equip(sets.tp[TP_Set_Names[TP_Index]]) 
+        elseif areas.towns:contains(world.area) then 
+            equip(sets.idle.town) 
+        else
+            equip(sets.idle.dt) 
+        end
+        
+        -- Confirms weapon selections update through transitions safely
+        if sets.wep and sets.wep[WEP_Set_Names[WEP_Index]] then
+            equip(sets.wep[WEP_Set_Names[WEP_Index]])
+        end
+    end
+end
 
+-- Registers the zone change hook cleanly after all functions are fully loaded
+my_zone_event = windower.register_event('zone change', handle_zone_gear_swap)
 
-
--------------------------
---   BLU Spells List   --
--------------------------
-
-blu_cure = S {'Healing Breeze', 'Magic Fruit', 'Plenilune Embrace', 'Pollen', 'Restoral', 'Wild Carrot'} 
-
-blu_phy = S {'Asuran Claws', 'Bilgestorm', 'Bludgeon', 'Body Slam', 'Feather Storm', 'Mandibular Bite', 'Queasyshroom', 'Power Attack', 'Ram Charge', 'Saurian Slide', 'Screwdriver', 'Sickle Slash', 
-'Smite of Rage', 'Spinal Cleave', 'Spiral Spin', 'Sweeping Gouge', 'Terror Touch', 'Battle Dance', 'Bloodrake', 'Death Scissors', 'Dimensional Death', 'Empty Thrash', 'Quadrastrike', 'Uppercut', 
-'Tourbillion', 'Thrashing Assault', 'Vertical Cleave', 'Whirl of Rage', 'Amorphic Spikes', 'Barbed Crescent', 'Claw Cyclone', 'Disseverment', 'Foot Kick', 'Frenetic Rip', 'Goblin Rush', 
-'Hysteric Barrage', 'Paralyzing Triad', 'Seedspray', 'Sinker Drill', 'Vanity Dive', 'Cannonball', 'Delta Thrust', 'Glutinous Dart', 'Grand Slam', 'Quad. Continuum', 'Sprout Smack', 
-'Benthic Typhoon', 'Helldive', 'Hydro Shot', 'Jet Stream', 'Pinecone Bomb', 'Wild Oats', 'Sweeping Gouge', 'Sudden Lunge'}
-
-blu_mab = S {'Acrid Stream', 'Anvil Lightning', 'Crashing Thunder', 'Charged Whisker', 'Droning Whirlwind', 'Firespit', 'Foul Waters', 'Gates of Hades', 'Leafstorm', 'Molting Plumage', 
-'Nectarous Deluge', 'Polar Roar', 'Regurgitation', 'Rending Deluge', 'Scouring Spate', 'Searing Tempest', 'Silent Storm', 'Spectral Floe', 'Subduction', 'Tem. Upheaval', 'Thermal Pulse', 
-'Thunderbolt', 'Uproot', 'Water Bomb', 'Atra. Libations', 'Blood Saber', 'Dark Orb', 'Death Ray', 'Eyes On Me', 'Blazing Bound', 'Evryone. Grudge', 'Palling Salvo', 'Tenebral Crush', 
-'Blinding Fulgor', 'Diffusion Ray', 'Ice Break', 'Magic Hammer', 'Rail Cannon', 'Retinal Glare', 'Sandspin', 'Vapor Spray'}
-				   
-blu_macc = S {'Dream Flower', 'Sheep Song','Feather Tickle','Light of Penance', 'MP Drainkiss', 'Entomb'}
-
-blu_skill = S {'Diamondhide', 'Metallic Body', 'Magic Barrier', 'Occultation', 'Atra. Libations', 'Digest', 'Blood Saber', 'Osmosis', 'Retinal Glare'}
-
-blu_rupt = S {'Barrier Tusk', 'Cocoon', 'Erratic Flutter', 'Harden Shell', 'Orcish Counterstance', 'Plasma Charge', 'Pyric Bulwark', 'Memento Mori', 'Nat. Meditation', 'Reactor Cool', 'Saline Coat', 
-'Feather Barrier', 'Refueling', 'Warm-Up', 'Zephyr Mantle', 'Reactor Cool', 'Plasma Charge', 'Amplification', 'Mighty Guard', 'Carcharian Verve', 'Magic Barrier'}
-					 
-blu_bre = S {'Bad Breath', 'Poison Breath', 'Radiant Breath', 'Frost Breath', 'Heat Breath', 'Thunder Breath', 'Wind Breath'}
+-- Safely cleans up the background listener when changing jobs or reloading
+function file_unload()
+    if my_zone_event then
+        windower.unregister_event(my_zone_event)
+        my_zone_event = nil
+        -- THIS LINE CONFIRMS THE UNLOAD HAPPENED:
+        windower.add_to_chat(121, '--- Gearswap cleanup: Zone event successfully removed! ---')
+    end
+end 
